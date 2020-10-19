2f52de5c7589586ccdc8564b4cb5233a9e440f2c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-10-19 10:58:41 +0300

    Initial repository translation.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["DataGenerator/Attribute/*.pyx",
+                           "DataGenerator/CorpusGenerator/*.pyx",
+                           "DataGenerator/InstanceGenerator/*.pyx",
+                           "DataGenerator/DatasetGenerator/*.pyx"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-DataGenerator-Cy',
+    version='1.0.0',
+    packages=['DataGenerator', 'DataGenerator.Attribute', 'DataGenerator.CorpusGenerator',
+              'DataGenerator.DatasetGenerator', 'DataGenerator.InstanceGenerator'],
+    url='https://github.com/olcaytaner/DataGenerator-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Classification dataset generator library for high level Nlp tasks',
+    install_requires=['NlpToolkit-AnnotatedSentence-Cy', 'NlpToolkit-AnnotatedTree-Cy', 'NlpToolkit-Classification-Cy',
+                      'NlpToolkit-MorphologicalDisambiguation-Cy']
+)
